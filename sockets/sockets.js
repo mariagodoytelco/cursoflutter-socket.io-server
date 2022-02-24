fa6cf23fdbbcc12c3c5e1ca8c415ba0399abe0d4
Maria L. Godoy <pasantetelco.maria@gmail.com>
2022-02-24 12:45:03 -0300

    mensajes y modelos

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,8 +1,20 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('BTS'));
+bands.addBand(new Band('Stray Kids'));
+bands.addBand(new Band('Seventeen'));
+bands.addBand(new Band('Got7'));
 
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log('cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => {
         console.log('cliente desconectado');
     });
@@ -13,6 +25,18 @@ io.on('connection', client => {
         console.log('mensaje: ',payload);
         //emitir evento a todos los clientes conectados
         io.emit('mensaje', {admin: 'nuevo mensaje'});
+    });
+
+    client.on('emitir-mensaje', ( payload ) => {
+        //console.log(payload);
+        //io.emit('nuevo-mensaje', payload); //esto emite a todos
+        //client.broadcast en vez de io para emitir a todos menos al que lo emitio
+        client.broadcast.emit('nuevo-mensaje', payload);
+    });
+
+    client.on('vote-band', (payload) =>{
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 
 });
