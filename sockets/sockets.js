3801fcef9fa91b4e7a62b24d5be4be15073f39ce
Maria L. Godoy <pasantetelco.maria@gmail.com>
2022-02-25 09:03:20 -0300

    cambios sobre bandas terminados

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -39,4 +39,15 @@ io.on('connection', client => {
         io.emit('active-bands', bands.getBands());
     });
 
+    client.on('add-band', (payload) =>{
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) =>{
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+    
+
 });
